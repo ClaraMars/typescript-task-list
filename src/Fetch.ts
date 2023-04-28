de2fb7a04ae155a170d0e9e4a23e9d736570be88
Clara Márquez <clara.marquez.torres@gmail.com>
2023-04-28 20:19:06 +0200

    Ejercicio 28.04

---
+++ src/Fetch.ts
@@ -0,0 +1,73 @@
+interface dataObj {
+  id: string,
+  title: string,
+  isDone: boolean
+}
+
+interface dataObjUpdate {
+  id: string,
+  title?: string,
+  isDone?: boolean
+}
+
+export class Fetch {
+  static BASE_URL: string = "http://localhost:8000/tasks";
+  static async getAll() {
+    const response: Response = await fetch("http://localhost:8000/tasks");
+    if (!response.ok) {
+      throw new Error(
+        `Error al obtener las tareas: ${response.status} ${response.statusText}`
+      );
+    }
+    const data: dataObj[] = await response.json();
+    return data;
+  }
+
+  //Añadir filas nuevas
+  static async create(task: dataObj[]) {
+    const response: Response = await fetch(this.BASE_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+    if (!response.ok) {
+      throw new Error(
+        `Error al crear la tarea: ${response.status} ${response.statusText}`
+      );
+    }
+    const data: dataObj[] = await response.json();
+    return data;
+  }
+
+  //Tipado parcial para actualizar solo ciertos campos
+  static async update(task: dataObjUpdate) {
+    const response: Response = await fetch(`${this.BASE_URL}${task.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+    if (!response.ok) {
+      throw new Error(
+        `Error al actualizar la tarea: ${response.status} ${response.statusText}`
+      );
+    }
+    const data: dataObj[] = await response.json();
+    return data;
+  }
+
+  //Eliminación de filas
+  static async delete(id: string) {
+    const response: Response = await fetch(`${this.BASE_URL}${id}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) {
+      throw new Error(
+        `Error al eliminar la tarea: ${response.status} ${response.statusText}`
+      );
+    }
+  }
+}
